5311a988d1b207c1dd1856bafd4276d8610c00b7
Rubendvb <rubenvasquez556@gmail.com>
2023-07-10 03:24:27 -0300

    fix: ajustado grid dos botões

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Menu from "./components/Menu.tsx";
-
-import Buttons from "./pages/Buttons.tsx";
-import Inputs from "./pages/Inputs.tsx";
-import Layout from "./components/Layout.tsx";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Menu />
-
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/buttons" element={<Buttons />} />
-          <Route path="/inputs" element={<Inputs />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Outlet />
+      <Footer />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App.tsx";
 
 import "normalize.css";
 import "./assets/css/Main.css";
+import Menu from "./components/Menu.tsx";
+import Buttons from "./pages/Buttons.tsx";
+import Inputs from "./pages/Inputs.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/buttons" element={<Buttons />} />
+          <Route path="/inputs" element={<Inputs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
