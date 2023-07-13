b8ddcf4feba26ecf5bba0e5bd5c50ade3220a086
Rubendvb <rubenvasquez556@gmail.com>
2023-07-13 03:19:58 -0300

    alinhando icons input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,30 @@
+import { InputHTMLAttributes } from "react";
 import "../assets/css/Input.css";
 
-export default function Input() {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string | undefined;
+  iconLeft?: boolean;
+  iconRight?: boolean;
+}
+
+export default function Input({ className, iconLeft, iconRight }: InputProps) {
+  const combinedInputClassName = `custom__input ${className || ""}`;
+
   return (
-    <>
-      <input className="custom__input" type="text" placeholder="Placeholder" />
-    </>
+    <div className={iconLeft || iconRight ? "containerInput" : undefined}>
+      {iconLeft ? (
+        <span className="material-icons icon-gray icon-phone">phone</span>
+      ) : null}
+
+      {iconRight ? (
+        <span className="material-icons icon-gray icon-lock">lock</span>
+      ) : null}
+
+      <input
+        className={combinedInputClassName}
+        type="text"
+        placeholder="Placeholder"
+      />
+    </div>
   );
 }
