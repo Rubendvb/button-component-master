8cf591145d905468635197a2f298621973026a55
Rubendvb <rubenvasquez556@gmail.com>
2023-07-15 03:03:47 -0300

    inputs textarea

--- src/pages/Inputs.tsx
+++ src/pages/Inputs.tsx
@@ -153,14 +153,14 @@ export default function Inputs() {
             </div>
           </div>
 
-          <div className="container__inputs input-size-three">
+          <div className="container__inputs input-container-width-full">
             <div>
               <span className="container__inputs__span">
                 {"<Input fullWidth />"}
               </span>
               <label>
                 Label
-                <Input />
+                <Input valueExist={true} className="input-width-full" />
               </label>
             </div>
           </div>
@@ -172,7 +172,7 @@ export default function Inputs() {
               </span>
               <label>
                 Label
-                <Input />
+                <Input className="input-textarea" />
               </label>
             </div>
           </div>
